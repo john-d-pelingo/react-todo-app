ecbd111eeebdab67e90b454327ac3649387ae266
John Darryl Pelingo <johndarrylpelingo@gmail.com>
2016-11-26 21:15:35 +0200

    Complete handleAddTodo

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -11,4 +11,18 @@ describe('TodoApp', () => {
     it('should exist', () => {
         expect(TodoApp).toExist();
     });
+
+    it('should add todo to the todos state on handleAddTodo', () => {
+        let todoText = 'Must test!';
+        let todoApp = TestUtils.renderIntoDocument(<TodoApp />);
+
+        todoApp.setState({
+            todos: []
+        });
+
+        // Simulate the handleAddTodo function
+        todoApp.handleAddTodo(todoText);
+
+        expect(todoApp.state.todos[0].text).toBe(todoText);
+    });
 });
