f2376e0b176fffbba8fc6fe05eaef519d14e4441
John Darryl Pelingo <johndarrylpelingo@gmail.com>
2016-12-02 02:24:37 +0200

    Fix logout

--- app/app.jsx
+++ app/app.jsx
@@ -37,7 +37,6 @@ firebase.auth().onAuthStateChanged((user) => {
         hashHistory.push('/todos');
     } else {
         store.dispatch(actions.logout());
-        store.dispatch(actions.removeTodos());
         hashHistory.push('/');
     }
 });
